336263907da6316b01bc9f48ee5091e4d8834d72
anand <anandpromax@gmail.com>
2024-03-21 14:27:01 +0530

    Refactor UserController.js

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,23 +1,23 @@
 const UserRepository = require("../repository/UserRepository");
-const { redis }= require("../config/redis");
+const { redis } = require("../config/redis");
 const axios = require("axios");
 
 const userRepository = new UserRepository();
 const create = async (req, res) => {
   try {
-    const { user_name, code_language, stdin, source_code, } = req.body;
-    
+    const { user_name, code_language, stdin, source_code } = req.body;
+
     if (!user_name || !code_language || !stdin || !source_code) {
       res.status(400).json({ message: "All fields are required" });
     }
     const language_data = {
-      "CPP": "52",
-      "Python": "92",
-      "java": "91",
-      "javaScript": "93",
+      CPP: "52",
+      Python: "92",
+      java: "91",
+      javaScript: "93",
     };
     const language_id = language_data[code_language];
-    
+
     const options = {
       method: "POST",
       url: "https://judge0-ce.p.rapidapi.com/submissions",
@@ -38,48 +38,38 @@ const create = async (req, res) => {
       },
     };
 
-    try {
-      const response = await axios.request(options);
-      const token = response.data.token.JSON.stringify();
-  if (!token) {
-    res.status(500).json({ message: "Error in token" });
-  } else {
-    const options = {
-      method: "GET",
-      url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
-      params: {
-        base64_encoded: "true",
-        fields: "*",
-      },
-      headers: {
-        "X-RapidAPI-Key": "f790f87593msh7fc75facc5e80cdp12dff0jsnd051cac37375",
-        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-      },
-    };
+    const response = await axios.request(options);
+    const token = response.data.token ;
+    if (!token) {
+      res.status(500).json({ message: "Error in token" });
+    } else {
+      const options = {
+        method: "GET",
+        url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+        params: {
+          base64_encoded: "true",
+          fields: "*",
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "f790f87593msh7fc75facc5e80cdp12dff0jsnd051cac37375",
+          "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+        },
+      };
 
-    try {
       const response = await axios.request(options);
-      const stdout = response.data.status.description.JSON.stringify();
+      const stdout = response.data.status.description;
       const { user_name, code_language, stdin, source_code } = req.body;
       // console.log(user_name, code_language, stdin, source_code, stdout);
-      const data = await userRepository.create(
-       { user_name,
+      const data = await userRepository.create({
+        user_name,
         code_language,
         stdin,
         stdout,
-        source_code}
-      );
+        source_code,
+      });
       res.status(201).json(data);
-    } catch (error) {
-      console.error(error);
     }
-  }
-
-      
-    } catch (error) {
-      console.error(error);
-    }
-  
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
